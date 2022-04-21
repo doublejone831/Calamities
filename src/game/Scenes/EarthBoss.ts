4739aec59075f3824b8e876ad5981b56f0aeb096
doublejone831 <12161990@inha.edu>
2022-04-21 14:46:52 -0400

    ignore

---
+++ src/game/Scenes/EarthBoss.ts
@@ -0,0 +1,197 @@
+import Vec2 from "../../Wolfie2D/DataTypes/Vec2";
+import AABB from "../../Wolfie2D/DataTypes/Shapes/AABB";
+import Sprite from "../../Wolfie2D/Nodes/Sprites/Sprite";
+import { CTCevent } from "./CTCEvent";
+import ElementController from "../Element/ElementController";
+import { Player_enums } from "../Player/Player_enums";
+import BaseStage from "./BaseStage";
+import PlayerController from "../Player/PlayerController";
+import Earth from "./Earth";
+export default class EarthBoss extends BaseStage {
+    protected endposition : Vec2;
+
+    loadScene(){
+        this.load.image("rock_S", "game_assets/sprites/rock_S.png");
+        this.load.image("rock_M", "game_assets/sprites/rock_M.png");
+        this.load.image("rock_L", "game_assets/sprites/rock_L.png");
+        this.load.image("rock_P", "game_assets/sprites/rock_P.png");
+        this.load.spritesheet("god", "game_assets/spritesheets/god.json");
+        this.load.spritesheet("element_equipped", "game_assets/spritesheets/element_equipped.json");
+        this.load.tilemap("level", "game_assets/tilemaps/earth.json");
+        this.load.object("board", "game_assets/data/earth_board.json");
+        this.load.image("portal", "game_assets/sprites/portal.png")
+        /*unlock all powers for testing
+        this.load.spritesheet("whirlwind", "game_assets/spritesheets/whirlwind.json");
+        this.load.image("gust", "game_assets/sprites/gust.png");
+        this.load.spritesheet("airstream", "game_assets/spritesheets/airstream.json");
+        this.load.image("bubble", "game_assets/sprites/bubble.png");
+        this.load.image("shallow_water", "game_assets/sprites/shallow_water.png");
+        this.load.spritesheet("ember", "game_assets/spritesheets/ember.json");
+        this.load.image("flames", "game_assets/sprites/flames.png");
+        this.load.image("ignite", "game_assets/sprites/ignite.png");
+        this.load.image("ice_cube", "game_assets/sprites/ice_cube.png");
+        this.load.spritesheet("torch", "game_assets/spritesheets/torch.json");
+        this.load.spritesheet("cursor", "game_assets/spritesheets/cursor.json");*/
+    }
+
+    unloadScene(): void {
+        this.load.keepImage("rock_M");
+        this.load.keepSpritesheet("god");
+        this.load.keepSpritesheet("element_equipped");
+        this.load.unloadAllResources();
+    }
+
+    startScene(){
+        super.startScene();
+        // Add in the tilemap
+        this.add.tilemap("level");
+
+        this.endposition.x = 16;
+        this.endposition.y = 15;
+
+        this.initializeGameboard();
+
+        this.initializePlayer();
+
+        this.elementGUI.animation.play("none_equipped");
+
+    }
+
+    updateScene(deltaT: number): void{
+        super.updateScene(deltaT);
+        while(this.receiver.hasNextEvent()){
+            let event = this.receiver.getNextEvent();
+
+            switch(event.type){
+                // CTC TODO: interacting and placing (if placing then have to account for the walls so you cant place there)
+                case CTCevent.INTERACT_ELEMENT:
+                    console.log("interact happened");
+                    console.log(event.data.get("positionX"));
+                    console.log(event.data.get("positionY"));
+                    var targetposX = event.data.get("positionX");
+                    var targetposY = event.data.get("positionY");
+                    var direction = event.data.get("direction");
+                    var target = this.gameboard[targetposX][targetposY];
+                    if(target != null) {
+                        this.pushRock(target, targetposX, targetposY, direction);
+                    }
+                    break;
+                case CTCevent.PLACE_ELEMENT:
+                    let placeX = event.data.get("positionX");
+                    let placeY = event.data.get("positionY");
+                    let type = event.data.get("type");
+                    if (placeX>=2 && placeX<=17 && placeY>=2 && placeY<=17) {
+                        let player_controller = (<PlayerController>this.player._ai);
+                        player_controller.cast_animation();
+                        if (this.gameboard[placeX][placeY] == null) {
+                            this.place_element(placeX, placeY, type);
+                        } else {
+                            switch(event.data.get("type")){
+                                case 1:
+                                    if(this.gameboard[placeX][placeY].imageId == "rock_P") {
+                                        let sprite = this.gameboard[placeX][placeY];
+                                        sprite.destroy();
+                                        this.gameboard[placeX][placeY] = null;
+                                        this.skillUsed[0] = false;
+                                    }
+                                    break;
+                                case 2:
+                                    if(this.gameboard[placeX][placeY].imageId== "whirlwind") {
+                                        let sprite = this.gameboard[placeX][placeY];
+                                        sprite.destroy();
+                                        this.gameboard[placeX][placeY] = null;
+                                        this.skillUsed[1] = false;
+                                    }
+                                    break;
+                                case 3:
+                                    if(this.gameboard[placeX][placeY].imageId == "bubble") {
+                                        let sprite = this.gameboard[placeX][placeY];
+                                        sprite.destroy();
+                                        this.gameboard[placeX][placeY] = null;
+                                        this.skillUsed[2] = false;
+                                    }
+                                    break;
+                                case 4:
+                                    if(this.gameboard[placeX][placeY].imageId == "ember") {
+                                        let sprite = this.gameboard[placeX][placeY];
+                                        sprite.destroy();
+                                        this.gameboard[placeX][placeY] = null;
+                                        this.skillUsed[3] = false;
+                                    }
+                                    break;
+                                case 5:
+                                    if(this.gameboard[placeX][placeY].imageId == "ice_cube") {
+                                        let sprite = this.gameboard[placeX][placeY];
+                                        sprite.destroy();
+                                        this.gameboard[placeX][placeY] = null;
+                                        this.skillUsed[4] = false;
+                                    }
+                                    break;
+                            }
+                            
+                        }
+                    }
+                    break;
+                case CTCevent.CHANGE_ELEMENT:
+                    switch(event.data.get("el")){
+                        case 1:
+                            this.elementSelected = 1;
+                            this.elementGUI.animation.play("earth_equipped");
+                            break;
+                        case 2:
+                            this.elementSelected = 2;
+                            this.elementGUI.animation.play("wind_equipped");
+                            break;
+                        case 3:
+                            this.elementSelected = 3;
+                            this.elementGUI.animation.play("water_equipped");
+                            break;
+                        case 4:
+                            this.elementSelected = 4;
+                            this.elementGUI.animation.play("fire_equipped");
+                            break;
+                        case 5:
+                            this.elementSelected = 5;
+                            this.elementGUI.animation.play("ice_equipped");
+                            break;
+                    }
+                    break;
+                case CTCevent.PLAYER_MOVE_REQUEST:
+                    var next = event.data.get("next");
+                    if(this.gameboard[next.x][next.y] == null || this.endposition == next){
+                        this.emitter.fireEvent(CTCevent.PLAYER_MOVE, {"scaling": 1});
+                        if(this.endposition.x === next.x && this.endposition.y === next.y){
+                            this.sceneManager.changeToScene(Earth, {});
+                        }
+                    }
+                    break;
+            }    
+        }
+    };
+
+    initializeGameboard(): void {
+        let boardData = this.load.getObject("board");
+
+        for (let i = 0; i < boardData.numElements; i++) {
+            let element = boardData.elements[i];
+            let sprite = this.add.sprite(element.type, "primary");
+            sprite.position.set(element.position[0]*16 + 8, element.position[1]*16 + 8);
+            sprite.addAI(ElementController, {});
+            this.gameboard[element.position[0]][element.position[1]] = sprite;
+        }
+    }
+
+    initializePlayer(): void {
+        this.player = this.add.animatedSprite("god", "primary");
+        this.player.animation.play("idle");
+        this.player.position.set(3*16+8, 3*16+8);
+        this.player.addPhysics(new AABB(Vec2.ZERO, new Vec2(8, 8)));
+        this.skillUsed = new Array(5).fill(false);
+        this.elementSelected = 0;
+        this.player.addAI(PlayerController, {tilemap: "Main", hasPower: [false,false,false,false,false]});
+    }
+
+    restartStage() {
+        this.sceneManager.changeToScene(EarthBoss, {});
+    }
+}
